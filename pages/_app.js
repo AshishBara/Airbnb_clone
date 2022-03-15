ff6182ee590b6924e51b0e2b48ac5bab30acbf3e
Ashish <ashish@Ashishs-MacBook-Air.local>
2022-03-15 12:02:25 +0530

    added next.cofig.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css';
-import '../styles/global.css';
+// import '../styles/global.css';
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from 'next/router';
 
